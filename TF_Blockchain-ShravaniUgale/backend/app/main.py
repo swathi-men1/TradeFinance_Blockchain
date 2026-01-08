b9eea4b8ebf2701e421f4465f7b88ba71209fe4b
Shravani Ugale <shravaniugale31@gmail.com>
2026-01-08 12:39:27 +0530

    Day 2: Connected PostgreSQL and added User model

--- TF_Blockchain-ShravaniUgale/backend/app/main.py
+++ TF_Blockchain-ShravaniUgale/backend/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from app.database import engine, Base
+from app.models.user import User
 
 app = FastAPI(
     title="ChainDocs – Trade Finance Blockchain Explorer",
     version="0.1.0"
 )
+
+Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def health_check():
