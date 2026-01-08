9b84a40ead3481c83940da3d942210cf3845ad4d
Ramya <mvramya2003@gmail.com>
2026-01-08 15:50:13 +0530

    Integrated PostgreSQL and document upload with metadata storage

--- models.py
+++ models.py
@@ -1,10 +1,21 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base
 
+# USERS TABLE
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+    name = Column(String)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+
+# DOCUMENTS TABLE
+class Document(Base):
+    __tablename__ = "documents"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String)
+    file_hash = Column(String)
+    owner_email = Column(String)  # simple link for now
