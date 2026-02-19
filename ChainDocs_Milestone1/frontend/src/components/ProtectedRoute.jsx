abe0959b07934a4fe911e1326f2c8f938b7b9df3
SAMAD-0101 <samads3samad@gmail.com>
2026-02-20 03:07:23 +0530

    Delete ChainDocs_Milestone1 directory

--- ChainDocs_Milestone1/frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({children}){
-  return localStorage.getItem("token") ? children : <Navigate to="/" />
-}
--- ChainDocs_Milestone1/frontend/src/pages/Dashboard.jsx
+++
@@ -1,4 +0,0 @@
-
-export default function Dashboard(){
-  return <h1>Milestone 1 Dashboard (Protected)</h1>
-}
