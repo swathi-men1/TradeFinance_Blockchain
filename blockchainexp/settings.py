ef498b0e1017524afa6891ac32ee105de402324e
Vangari Akshitha <akshithavangari1@gmail.com>
2026-02-21 18:20:09 +0530

    Update settings.py

--- blockchainexp/settings.py
+++ blockchainexp/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'blockchainexp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tradefinance_db',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
