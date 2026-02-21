c14f22c8971ae46e7da8c43caeade2d7fd12edbb
Gangavangala07 <gangavangala754@gmail.com>
2026-02-21 17:30:38 +0530

    Revert "Tf blockchain gangavangala"

--- Trade_Finance_Blockchain_/services/blockchain_service.py
+++
@@ -1,4 +0,0 @@
-import hashlib
-
-def generate_hash(file_bytes: bytes):
-    return hashlib.sha256(file_bytes).hexdigest()
--- Trade_Finance_Blockchain_/services/risk_service.py
+++
@@ -1,9 +0,0 @@
-def calculate_risk_score(doc_count: int, tx_count: int):
-    score = 100
-
-    if doc_count < 3:
-        score -= 30
-    if tx_count < 2:
-        score -= 20
-
-    return max(score, 0)
