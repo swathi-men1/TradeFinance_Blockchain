b9d0aeb4579c4ce15e4d1634e7b07b4c873ac286
Gangavangala07 <gangavangala754@gmail.com>
2026-02-21 17:36:41 +0530

    Revert "Tf blockchain gangavangala"

--- Trade_Finance_Blockchain_/static/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-fetch("/")
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("docs").innerText = data.total_documents;
-    document.getElementById("txns").innerText = data.active_transactions;
-    document.getElementById("verified").innerText = data.verified_today;
-    document.getElementById("risk").innerText = data.avg_risk_score;
-  });
